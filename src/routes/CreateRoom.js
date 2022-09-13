86a867727364f7a72852c0f7621842607ee64e69
divya-123 <divyasrivastava1113@gmail.com>
2022-09-13 20:13:34 +0530

    initial commit

---
+++ src/routes/CreateRoom.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { v1 as uuid } from "uuid";
+import { useNavigate } from "react-router-dom";
+
+const CreateRoom = (props) => {
+    let navigate = useNavigate();
+    function create() {
+        const id = uuid();
+        console.log(id);
+        // props.history.push(`/room/${id}`);
+        navigate(`/room/${id}`);
+      
+    }
+
+    return (
+        <button onClick={create}>Create Room</button>
+    );
+}
+
+export default CreateRoom;
